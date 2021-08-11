035dc0b364c7d1a0458364002c3a12ddc0e5cedf
changan <2868310944@qq.com>
2021-08-11 17:22:24 +0800

    small-spring

--- small-spring-step-05/src/main/java/com/yica/springframework/beans/factory/config/AutowireCapableBeanFactory.java
+++ small-spring-step-05/src/main/java/com/yica/springframework/beans/factory/config/AutowireCapableBeanFactory.java
@@ -3,7 +3,7 @@ package com.yica.springframework.beans.factory.config;
 import com.yica.springframework.beans.factory.BeanFactory;
 
 /**
- * Extension of the {@link cn.bugstack.springframework.beans.factory.BeanFactory}
+ * Extension of the {@link com.yica.springframework.beans.factory.BeanFactory}
  * interface to be implemented by bean factories that are capable of
  * autowiring, provided that they want to expose this functionality for
  * existing bean instances.
